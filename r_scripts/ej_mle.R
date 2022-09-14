e17a0597377891a359efb84db1c1711854f354da
J.E. Alcalá <jaime.atemores@alumnos.udg.mx>
2022-09-13 20:21:51 -0500

    Create ej_mle.R

---
+++ r_scripts/ej_mle.R
@@ -0,0 +1,41 @@
+par(mgp = c(1.2, 0.25, 0))
+options(repr.plot.width = 7, repr.plot.height = 5)
+k <- 9
+n <- 10
+theta_1 <- 0.5
+theta_2 <- 0.9
+like_binom_plot(k, n, theta_2, theta_1)
+
+# grid-search
+
+par(mgp = c(1.2, 0.25, 0))
+options(repr.plot.width = 7, repr.plot.height = 5)
+k <- 9
+n <- 10
+theta_1 <- 0.5
+theta_2 <- 0.9
+like_binom_plot(k, n, theta_2, theta_1)
+
+# En la práctica, este método puede ser muy costoso. Imaginar, por ejemplo, 
+# que se tiene que hacer una búsqueda para estimar 2 parámetros. Tendríamos que 
+# hacer combinaciones del grid de ambos parámetros. Este suele ser el método de 
+# fuerza bruta que se usa, en ocasiones, como primera aproximación o cuando no nos 
+# funciona ninguna otra cosa.
+#
+# Es más eficiente usar un algoritmo de optimización, como `optimize()`. Tiene 
+# los siguientes argumentos:
+
+?optimize
+
+binom_mle <- function(k, n) {
+  L <- function(theta) log(dbinom(k, n, theta))
+  # usar solo interval entre 0 y 1 para theta
+  # dado que buscamos un máximo, declaramos TRUE
+  mle <- optimize(L, interval = c(0, 1), maximum = TRUE)
+  mle
+}
+mle_theta <- binom_mle(9, 10)
+mle_theta
+# retorna dos valores: el valor que maximiza, y la fn objetivo (L)
+# maximum corresponde al MLE, theta_hat. El valor de la objetivo
+# variará en con respecto al valor encontrado antes porque omitimos el coeficiente binomial
