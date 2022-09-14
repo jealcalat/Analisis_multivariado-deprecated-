2c3d237ff5435f664921154641f7670d82342db2
J.E. Alcalá <jaime.atemores@alumnos.udg.mx>
2022-09-14 12:28:19 -0500

    xournalpp

--- r_scripts/ej_mle.R
+++ r_scripts/ej_mle.R
@@ -3,18 +3,22 @@ options(repr.plot.width = 7, repr.plot.height = 5)
 k <- 9
 n <- 10
 theta_1 <- 0.5
-theta_2 <- 0.9
+theta_2 <- 0.7
 like_binom_plot(k, n, theta_2, theta_1)
 
 # grid-search
 
-par(mgp = c(1.2, 0.25, 0))
-options(repr.plot.width = 7, repr.plot.height = 5)
-k <- 9
-n <- 10
-theta_1 <- 0.5
-theta_2 <- 0.9
-like_binom_plot(k, n, theta_2, theta_1)
+theta <- seq(0, 1, 0.0001)
+
+L <- dbinom(9, 10, theta)
+plot(theta, L)
+
+ind_max <- which.max(L)
+
+theta[ind_max]
+
+plot(theta, L)
+
 
 # En la práctica, este método puede ser muy costoso. Imaginar, por ejemplo, 
 # que se tiene que hacer una búsqueda para estimar 2 parámetros. Tendríamos que 
@@ -26,6 +30,14 @@ like_binom_plot(k, n, theta_2, theta_1)
 # los siguientes argumentos:
 
 ?optimize
+k <- 9
+n <- 10
+
+L <- function(theta) dbinom(k, n, theta)
+mle <- optimize(L, interval = c(0, 1), maximum = TRUE)
+
+print(mle)
+
 
 binom_mle <- function(k, n) {
   L <- function(theta) log(dbinom(k, n, theta))
